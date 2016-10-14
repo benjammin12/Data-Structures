1f181fa75a6faba081f7434950c9346912b466a5
Benjamin <benjammin12@users.noreply.github.com>
2016-10-14 17:13:12 -0400

    Update App.java

--- App.java
+++ App.java
@@ -2,21 +2,19 @@
 public class App {
 
 	public static void main(String[] args) {
-		Queue queue = new Queue(5);
+		ReverseWord greeting = new ReverseWord();
+		greeting.insertToTop('h');
+		greeting.insertToTop('e');
+		greeting.insertToTop('l');
+		greeting.insertToTop('l');
+		greeting.insertToTop('o');
 		
-		queue.insertFromRear(10); //fill up array
-		queue.insertFromRear(11);
-		queue.insertFromRear(12);
-		queue.insertFromRear(3);
-		queue.insertFromRear(9);
 		
-		queue.removeFromFront(); //remove 3 elements
-		queue.removeFromFront();
-		queue.removeFromFront();
+		while(! greeting.isEmpty()){
+			System.out.println((char)(greeting.removeFromTop())); //you can type cast a character
+		}
 		
-		queue.insertFromRear(5); //confirm that you could put back those 3 elements and wrap araound works
-		queue.insertFromRear(7);
-		queue.insertFromRear(21);
 	}
 
 }
+
